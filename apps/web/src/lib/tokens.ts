828e081033c46280bf89ed143d3bc61ebdf8c862
abeachmad <abeachmad@users.noreply.github.com>
2025-11-24 15:17:47 +0800

    fix: Add isNative property to all tokens for TypeScript

--- apps/web/src/lib/tokens.ts
+++ apps/web/src/lib/tokens.ts
@@ -9,26 +9,31 @@ export const SUPPORTED_TOKENS = {
     address: '0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814',
     symbol: 'tBUSD',
     decimals: 18,
+    isNative: false,
   },
   tUSDT: {
     address: '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd',
     symbol: 'tUSDT',
     decimals: 18,
+    isNative: false,
   },
   tUSDC: {
     address: '0x64544969ed7EBf5f083679233325356EbE738930',
     symbol: 'tUSDC',
     decimals: 18,
+    isNative: false,
   },
   tDAI: {
     address: '0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867',
     symbol: 'tDAI',
     decimals: 18,
+    isNative: false,
   },
   tBTC: {
     address: '0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8',
     symbol: 'tBTC',
     decimals: 18,
+    isNative: false,
   },
 } as const;
 
